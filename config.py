a8cc364dfa1f3a8bbfb336ac7582b76a051b1b79
Nirmal Dhara <nirmal.dhara@gmail.com>
2024-11-18 22:50:56 +0530

    Update config.py
    removed key

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ DB_USER = "root"
 DB_PASSWORD = "root"
 DB_NAME = "quiz_app"
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = ""
